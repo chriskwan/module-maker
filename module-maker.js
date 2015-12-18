41b8bcabe3d91c04d095aeb053f50a160893c3d7
Christopher Kwan <ckwan@vistaprint.com>
2015-12-17 19:40:55 -0500

    Fix issues with ModuleMaker overriding the module it is trying to make when it makes itself

--- module-maker.js
+++ module-maker.js
@@ -1,27 +1,53 @@
+"use strict";
+
 // These checks based on those in underscore.js: http://underscorejs.org/docs/underscore.html
 
-var makeCommonJS = function (obj, name) {
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = obj;
-        return true;
-    } else if (typeof exports !== 'undefined' && name) {
-        exports[name] = obj;
+// Make a CommonJS module out of the object
+// Returns true if a module was successfully created
+// NOTE: Important to pass in a reference to the `module` object from the caller's scope
+// otherwise ModuleMaker would use the `module` object in its own scope
+var makeCommonJS = function (options) {
+    var obj = options.obj;
+    var moduleObj = options.moduleObj;
+    var name = options.name;
+
+    if (typeof moduleObj !== 'undefined' && moduleObj.exports) {
+        moduleObj.exports = obj;
         return true;
     }
+
     return false;
 };
 
-var makeAmd = function (obj, name) {
+// Make an AMD module out of the object
+// Returns true if a module was successfully created
+var makeAMD = function (options) {
+    var obj = options.obj;
+    var name = options.name;
+
     if (typeof define === 'function' && define.amd) {
-        define(name, [], function(){
-            return obj;
-        });
+        if (name) {
+            define(name, [], function () {
+                return obj;
+            });
+        } else {
+            define([], function () {
+                return obj;
+            });
+        }
         return true;
     }
+
     return false;
 };
 
-var makeGlobal = function (obj, name) {
+// Store the object as a global variable
+// Returns true if a global variable was successfully created
+// (only recommended if you cannot make an actual module)
+var makeGlobal = function (options) {
+    var obj = options.obj;
+    var name = options.name;
+
     var globalObj = null;
     if (typeof global !== 'undefined') {
         globalObj = global;
@@ -33,27 +59,42 @@ var makeGlobal = function (obj, name) {
         globalObj[name] = obj;
         return true;
     }
+
     return false;
 };
 
-var make = function (obj, name) {
-    var isCommonJSMade = makeCommonJS(obj, name);
-    var isAmdMade = makeAmd(obj, name);
+// Try to create both a CommonJS and an AMD module from the object
+// If neither could be made, try to store it as a global variable
+// Returns true if a module was successfully made by any means
+// options:
+// - obj: the object to make into a module
+// - moduleObj: a reference to the caller's module object (just pass in the word module)
+// - name: used for AMD (optional) and global objects (required)
+var make = function (options) {
+    var isCommonJSMade = makeCommonJS(options);
+    var isAMDMade = makeAMD(options);
 
-    var isModuleMade = isCommonJSMade || isAmdMade;
+    var isModuleMade = isCommonJSMade || isAMDMade;
 
     // Only resort to making a global if you could not make a proper module
     if (!isModuleMade) {
-        makeGlobal(obj, name);
+        return makeGlobal(options);
     }
+
+    return isModuleMade;
 };
 
 var ModuleMaker = {
     make: make,
     makeCommonJS: makeCommonJS,
-    makeAmd: makeAmd,
+    makeAMD: makeAMD,
     makeGlobal: makeGlobal
 };
 
-// Eat own dogfood
-make(ModuleMaker);
+// Eat own dogfood:
+// ModuleMaker uses its own methods to make itself into a module :)
+ModuleMaker.make({
+    obj: ModuleMaker,
+    moduleObj: module,
+    name: "ModuleMaker" // Important to specify name. Otherwise, this will override other anonymous AMD modules
+});
